3d7f80d6ef14f3c6dfe31128f64c3cc9712ebed8
Matt Fugere <mjfugere@gmail.com>
2020-03-01 11:10:36 -0500

    Removed Nightwatch E2E Updated unit tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from "@vue/test-utils"
+import Menu from "@/components/Menu.vue"
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+describe("Menu.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message"
+    const wrapper = shallowMount(Menu, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
