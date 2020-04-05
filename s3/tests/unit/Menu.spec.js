381cc25890d16093ca3a4cf01df50f74763e852d
Matt Fugere <mjfugere@gmail.com>
2020-04-05 00:42:26 -0400

    Updating unit tests and Jest config

--- s3/tests/unit/Menu.spec.js
+++ s3/tests/unit/Menu.spec.js
@@ -2,11 +2,8 @@ import { shallowMount } from "@vue/test-utils"
 import Menu from "@/components/Menu.vue"
 
 describe("Menu.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message"
-    const wrapper = shallowMount(Menu, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+  it("is a Vue instance", () => {
+    const wrapper = shallowMount(Menu)
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
