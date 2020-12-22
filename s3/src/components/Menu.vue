506075ec3a3dec3fb1746e2471d6ec3f0f0cb704
Matt Fugere <mjfugere@gmail.com>
2020-12-22 16:21:53 -0500

    Character name display under menu card

--- s3/src/components/Menu.vue
+++ s3/src/components/Menu.vue
@@ -5,12 +5,16 @@
       <h2>My Characters</h2>
       <ul class="list-group list-group-horizontal" v-if="account && Object.keys(account).length > 0">
         <template v-for="(character, index) in account.CHARACTERS">
-          <li class="list-group-item borderless" :key="character.REF">
-            <img src="@/assets/menu_icon_char.png" alt="character.NAME" @click="editCharacter('view/' + index + character.REF)">
+          <li class="list-group-item borderless card content-card text-dark bg-light" :key="character.REF">
+            <img class="card-img-top" src="@/assets/menu_icon_char.png" alt="character.NAME"
+              @click="editCharacter('view/' + index + character.REF)">
+            <div class="card-body">{{character.NAME}}</div>
           </li>
         </template>
-        <li class="list-group-item borderless">
-          <img src="@/assets/menu_icon_add.png" alt="Create a new character" @click="editCharacter('create')">
+        <li class="list-group-item borderless card content-card text-dark bg-light">
+          <img class="card-img-top" src="@/assets/menu_icon_add.png" alt="Create a new character"
+            @click="editCharacter('create')">
+          <div class="card-body">Create a new character</div>
         </li>
       </ul>
     </div>
@@ -18,18 +22,30 @@
       <h2>My Campaigns</h2>
       <ul class="list-group list-group-horizontal" v-if="account && Object.keys(account).length > 0">
         <template v-for="campaign in createdCampaigns">
-          <li class="list-group-item borderless" :key="campaign.ref"><img src="@/assets/menu_icon_game.png" alt="campaign.name"></li>
+          <li class="list-group-item borderless card content-card text-dark bg-light" :key="campaign.REF">
+            <img class="card-img-top" src="@/assets/menu_icon_game.png" alt="campaign.NAME">
+            <div class="card-body">{{campaign.NAME}}</div>
+          </li>
         </template>
-        <li class="list-group-item borderless"><img src="@/assets/menu_icon_add.png" alt="Create a new campaign"></li>
+        <li class="list-group-item borderless card content-card text-dark bg-light">
+          <img class="card-img-top" src="@/assets/menu_icon_add.png" alt="Create a new campaign">
+          <div class="card-body">Create a new campaign</div>
+        </li>
       </ul>
     </div>
     <div>
       <h2>Join a Campaign</h2>
       <ul class="list-group list-group-horizontal" v-if="account && Object.keys(account).length > 0">
         <template v-for="campaign in joinedCampaigns">
-          <li class="list-group-item borderless" :key="campaign.ref"><img src="@/assets/menu_icon_game.png" alt="campaign.name"></li>
+          <li class="list-group-item borderless card content-card text-dark bg-light" :key="campaign.REF">
+            <img class="card-img-top" src="@/assets/menu_icon_game.png" alt="campaign.NAME">
+            <div class="card-body">{{campaign.NAME}}</div>
+          </li>
         </template>
-        <li class="list-group-item borderless"><img src="@/assets/menu_icon_add.png" alt="Join a new campaign"></li>
+        <li class="list-group-item borderless card content-card text-dark bg-light">
+          <img class="card-img-top" src="@/assets/menu_icon_add.png" alt="Join a new campaign">
+          <div class="card-body">Join a new campaign</div>
+        </li>
       </ul>
     </div>
   </section>
@@ -62,5 +78,8 @@ export default {
 <style>
 li.borderless {
   border: 0 none;
+}
+.content-card {
+  width: 12em;
 }
 </style>
